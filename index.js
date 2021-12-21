3ade57218778f8b58eaf06fc0f14e94e13486f52
msunglim <mslim1210@gmail.com>
2021-12-21 18:26:17 +0900

    successfully uploaded

--- index.js
+++ index.js
@@ -6,14 +6,35 @@ const app = express();
 //can be 3000,4000,5000,,
 const port = 5000;
 
+const { User } = require('./modules/User')
+const bodyParser = require('body-parser')
+
+//body parse can analysize application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: true }))
+//body parse analysize json type
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Moungsung:msung1@msunglim.qgxrz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+const config = require('./config/key')
+mongoose.connect(config.mongoURI)
     .then(() => console.log('connected to MongoDB')).catch(err => console.log(err))
 
 
 
 //when arrive at root dir, print hello world
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Hello World nodemon makes me auto compile? yeah u just need to refresh the website!'))
 
-//start at 5000.
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//end point: register callbackfunction is (req,res)=>{}
+app.post('/register', (req, res) => {
+    //when signing up, if we get every neccessary information from client,
+    //put them into DB
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            sucess: true
+        })
+    })
+})
+
+app.listen(port, () =>  console.log(`Example app listening on port ${port}!`) )
